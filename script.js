12f606ebae7a6787adc0b59dcc012c01c276a9ab
Aishat Yusuff <aishat.yusuff@ibm.com>
2023-08-12 10:56:04 +0100

    removes nowrap

--- script.js
+++ script.js
@@ -5,9 +5,6 @@ const toggle = document.querySelector(".breadcrumb");
 const nav = document.querySelector(".navigation");
 const main = document.querySelector("main");
 
-// toggle.onclick = function () {
-//   nav.classList.toggle("active");
-// };
 toggle.addEventListener("click", function () {
   nav.classList.toggle("active");
   main.classList.toggle("active");
